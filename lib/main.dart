36261d456406f9878781cdacb664f44b4aa7b174
TheYautja <am.96138669@gmail.com>
2026-04-15 16:57:27 -0300

    add template

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Curriculo());
+}
+
+
+class Curriculo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("o tal do bagual"),
+        backgroundColor: Color(0xFF78a388),
+      )
+    )
+  }
+
+}
